e41fccb7d28fdb65af1206e90fae110676be9dad
Lipnevich <Nicolay.Lipnevich@gmail.com>
2018-02-25 19:22:52 +0300

    New translations were added

--- app/src/main/java/by/nicolay/lipnevich/noxbox/tools/I18nizer.java
+++ app/src/main/java/by/nicolay/lipnevich/noxbox/tools/I18nizer.java
@@ -5,17 +5,13 @@ import android.support.annotation.RequiresApi;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
