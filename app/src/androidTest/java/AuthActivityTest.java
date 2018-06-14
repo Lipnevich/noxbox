d0c09918762a122de3fe81b58368132d742a42d2
VladislawKravchenok <vladviva5991@gmail.com>
2018-06-14 17:05:10 +0300

    Страница входа - тесты

---
+++ app/src/androidTest/java/AuthActivityTest.java
@@ -0,0 +1,46 @@
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import by.nicolay.lipnevich.noxbox.R;
+import by.nicolay.lipnevich.noxbox.pages.AuthPage;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+public class AuthActivityTest {
+
+    @Rule
+    public ActivityTestRule<AuthPage> activityTestRule =
+            new ActivityTestRule<>(AuthPage.class);
+
+    @Test
+    public void checkContainerIsEnabled() {
+        onView(withId(R.id.checkbox)).perform(click());
+        onView(withId(R.id.googleAuth)).check(matches(isEnabled()));
+        onView(withId(R.id.phoneAuth)).check(matches(isClickable()));
+    }
+
+    @Test
+    public void checkContainerIsDisabled() {
+        onView(withId(R.id.googleAuth)).check(matches(isDisplayed()));
+        onView(withId(R.id.phoneAuth)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkContainerIsClickable() {
+        onView(withId(R.id.checkbox)).perform(click());
+        onView(withId(R.id.googleAuth)).check(matches(isClickable()));
+        onView(withId(R.id.phoneAuth)).check(matches(isClickable()));
+    }
+}
