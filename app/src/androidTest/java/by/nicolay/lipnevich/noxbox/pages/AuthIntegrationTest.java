98729e1f089fcbb3c46cca3cbc650c7ad968929a
Lipnevich <Nicolay.Lipnevich@gmail.com>
2018-06-16 19:24:11 +0300

    Test for google login and logout

--- app/src/androidTest/java/by/nicolay/lipnevich/noxbox/pages/AuthIntegrationTest.java
+++ app/src/androidTest/java/by/nicolay/lipnevich/noxbox/pages/AuthIntegrationTest.java
@@ -1,32 +1,25 @@
 package by.nicolay.lipnevich.noxbox.pages;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiSelector;
-import android.view.View;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.regex.Matcher;
-
 import by.nicolay.lipnevich.noxbox.R;
-import by.nicolay.lipnevich.noxbox.pages.AuthPage;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.not;
 
 
@@ -36,24 +29,36 @@ public class AuthIntegrationTest {
     @Rule
     public ActivityTestRule<AuthPage> rule = new ActivityTestRule<>(AuthPage.class);
 
-    private UiDevice device;
-
-    @Before
-    public void before() throws Exception {
-        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-    }
-
     @Test
     public void testGoogleSignIn() throws Exception {
         Thread.sleep(1000);
         onView(withId(R.id.checkbox)).check(matches(not(isChecked())));
         onView(withId(R.id.googleAuth)).check(matches(not(isEnabled())));
         onView(withId(R.id.checkbox)).perform(click());
-        device.findObject(new UiSelector().resourceId("by.nicolay.lipnevich.noxbox:id/googleAuth"))
-                .clickAndWaitForNewWindow();
+        getItemById("googleAuth").clickAndWaitForNewWindow();
 
-        UiObject account = device.findObject(new UiSelector().textContains("Nicolay.Lipnevich@Gmail.com"));
-        account.click();
+        UiObject account = getItemByText("Nicolay.Lipnevich@Gmail.com");
+        if(account.exists()) {
+            account.clickAndWaitForNewWindow();
+        }
+
+        Thread.sleep(5000);
+        getItemById("menu").click();
+
+        onView(withText("Logout")).perform(click());
+    }
+
+    private UiObject getItemByText(String text) {
+        return getDevice().findObject(new UiSelector().textContains(text));
+    }
+
+    private UiObject getItemById(String id) {
+        return getDevice().findObject(new UiSelector().resourceId(rule.getActivity().getPackageName()
+                + ":id/" + id));
+    }
+
+    private UiDevice getDevice() {
+        return UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
     }
 
 }
--- app/src/main/java/by/nicolay/lipnevich/noxbox/tools/FragmentManager.java
+++ app/src/main/java/by/nicolay/lipnevich/noxbox/tools/FragmentManager.java
@@ -7,10 +7,12 @@ import android.app.Fragment;
 public class FragmentManager {
 
     public static void createFragment(Activity activity, Fragment fragment, int layout){
+        if(activity == null || fragment == null) return;
         activity.getFragmentManager().beginTransaction().replace(layout,fragment).commit();
     }
 
     public static void removeFragment(Activity activity,Fragment fragment){
+        if(activity == null || fragment == null) return;
         activity.getFragmentManager().beginTransaction().remove(fragment).commit();
     }
 
