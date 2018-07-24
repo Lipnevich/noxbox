db35efeaebd7b2b20bab3e85fe90527377583d95
VladislawKravchenok <vladviva5991@gmail.com>
2018-07-24 20:57:39 +0300

    service price display; detail page modification;

--- app/src/main/java/by/nicolay/lipnevich/noxbox/detailed/DetailedNoxboxPage.java
+++ app/src/main/java/by/nicolay/lipnevich/noxbox/detailed/DetailedNoxboxPage.java
@@ -3,8 +3,8 @@ package by.nicolay.lipnevich.noxbox.detailed;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,16 +15,15 @@ import com.bumptech.glide.request.RequestOptions;
 import by.nicolay.lipnevich.noxbox.R;
 import by.nicolay.lipnevich.noxbox.model.Profile;
 import by.nicolay.lipnevich.noxbox.state.State;
-import by.nicolay.lipnevich.noxbox.tools.DebugMessage;
 import by.nicolay.lipnevich.noxbox.tools.Task;
 
-public class DetailedNoxboxPage extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+public class DetailedNoxboxPage extends AppCompatActivity {
 
-    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.9f;
-    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS     = 0.3f;
-    private static final int ALPHA_ANIMATIONS_DURATION              = 200;
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f;
+    private static final int ALPHA_ANIMATIONS_DURATION = 200;
 
-    private boolean mIsTheTitleVisible          = false;
+    private boolean mIsTheTitleVisible = false;
     private boolean mIsTheTitleContainerVisible = true;
 
     private LinearLayout mTitleContainer;
@@ -38,30 +37,34 @@ public class DetailedNoxboxPage extends AppCompatActivity implements AppBarLayou
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_description);
-
-        init();
-
-        mAppBarLayout.addOnOffsetChangedListener(this);
-
-        startAlphaAnimation(mTitle, 0, View.INVISIBLE);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
         State.listenProfile(new Task<Profile>() {
             @Override
             public void execute(Profile profile) {
+                init();
                 draw(profile);
             }
         });
     }
 
-    private void draw(Profile profile){
-        DebugMessage.popup(this,getResources().getString(profile.getViewed().getType().getName()));
-        drawTypeIcon(profile);
+    private void draw(Profile profile) {
+        drawToolbar(profile);
+        drawNoxboxPrice(profile);
+        //drawTypeIcon(profile);
     }
-    private void drawTypeIcon(Profile profile){
+
+    private void drawToolbar(Profile profile) {
+        setSupportActionBar(((Toolbar) findViewById(R.id.toolbar)));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(profile.getViewed().getType().getName());
+    }
+
+    private void drawNoxboxPrice(Profile profile) {
+        ((TextView) findViewById(R.id.noxboxPriceView)).setText(profile.getCurrent().getPrice());
+    }
+
+
+
+    private void drawTypeIcon(Profile profile) {
         Glide.with(getApplicationContext())
                 .asBitmap()
                 //.load(profile.getViewed().getType().getImage())
@@ -72,17 +75,16 @@ public class DetailedNoxboxPage extends AppCompatActivity implements AppBarLayou
     }
 
     private void init() {
-        mTitle          = (TextView) findViewById(R.id.toolbarTextView);
-        mTitleContainer = (LinearLayout) findViewById(R.id.titleContainerLayout);
-        mAppBarLayout   = (AppBarLayout) findViewById(R.id.appBarLayout);
-        mTypeImageView   = (ImageView) findViewById(R.id.typeImageView);
-
+        //mTitle = (TextView) findViewById(R.id.toolbarTextView);
+        // mTitleContainer = (LinearLayout) findViewById(R.id.titleContainerLayout);
+        //mAppBarLayout = (AppBarLayout) findViewById(R.id.appBarLayout);
+        // mTypeImageView = (ImageView) findViewById(R.id.typeImageView);
 
 
         //TODO need find background picture for all types..
-        mBackImageTypeView   = (ImageView) findViewById(R.id.backgroundImageTypeView);
+        // mBackImageTypeView = (ImageView) findViewById(R.id.backgroundImageTypeView);
 
-                findViewById(R.id.acceptButton).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.acceptButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 State.listenProfile(new Task<Profile>() {
@@ -108,55 +110,5 @@ public class DetailedNoxboxPage extends AppCompatActivity implements AppBarLayou
         });
     }
 
-    @Override
-    public void onOffsetChanged(AppBarLayout appBarLayout, int offset) {
-        int maxScroll = appBarLayout.getTotalScrollRange();
-        float percentage = (float) Math.abs(offset) / (float) maxScroll;
 
-        handleAlphaOnTitle(percentage);
-        handleToolbarTitleVisibility(percentage);
-    }
-
-    private void handleToolbarTitleVisibility(float percentage) {
-        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
-
-            if(!mIsTheTitleVisible) {
-                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
-                mIsTheTitleVisible = true;
-            }
-
-        } else {
-
-            if (mIsTheTitleVisible) {
-                startAlphaAnimation(mTitle, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                mIsTheTitleVisible = false;
-            }
-        }
-    }
-
-    private void handleAlphaOnTitle(float percentage) {
-        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
-            if(mIsTheTitleContainerVisible) {
-                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
-                mIsTheTitleContainerVisible = false;
-            }
-
-        } else {
-
-            if (!mIsTheTitleContainerVisible) {
-                startAlphaAnimation(mTitleContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
-                mIsTheTitleContainerVisible = true;
-            }
-        }
-    }
-
-    public static void startAlphaAnimation (View v, long duration, int visibility) {
-        AlphaAnimation alphaAnimation = (visibility == View.VISIBLE)
-                ? new AlphaAnimation(0f, 1f)
-                : new AlphaAnimation(1f, 0f);
-
-        alphaAnimation.setDuration(duration);
-        alphaAnimation.setFillAfter(true);
-        v.startAnimation(alphaAnimation);
-    }
 }
