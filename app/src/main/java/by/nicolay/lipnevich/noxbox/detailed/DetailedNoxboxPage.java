d166a25d69c92aa6f34f72736f5d847376bccc51
VladislawKravchenok <vladviva5991@gmail.com>
2018-07-30 11:36:08 +0300

    change vector arrow in detailed page

--- app/src/main/java/by/nicolay/lipnevich/noxbox/detailed/DetailedNoxboxPage.java
+++ app/src/main/java/by/nicolay/lipnevich/noxbox/detailed/DetailedNoxboxPage.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -66,33 +68,36 @@ public class DetailedNoxboxPage extends AppCompatActivity {
         setSupportActionBar(toolbar);
         final Drawable backArrow = getResources().getDrawable(R.drawable.arrow_back);
         //upArrow.setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
-       // getSupportActionBar().setHomeAsUpIndicator(backArrow);
+        // getSupportActionBar().setHomeAsUpIndicator(backArrow);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(noxbox.getType().getName());
 
     }
 
-    private void drawDescription(Noxbox noxbox){
-        ((ImageView)findViewById(R.id.typeImage)).setImageResource(R.drawable.information);
-        ((TextView)findViewById(R.id.descriptionTitle)).setText(getResources().getString(R.string.description));
-        if(noxbox.getRole() == MarketRole.supply){
-            ((TextView)findViewById(R.id.previousDescription)).setText("Готов предоставить услугу:");
-        }else{//TODO (vl) transfer text to xml
-            ((TextView)findViewById(R.id.previousDescription)).setText("Хочу получить услугу:");
+    private void drawDescription(Noxbox noxbox) {
+        drawDropdownElement(R.id.descriptionTitleLayout, R.id.descriptionLayout);
+        changeArrowVector(R.id.descriptionLayout, R.id.descriptionArrow);
+        ((ImageView) findViewById(R.id.typeImage)).setImageResource(R.drawable.information);
+        ((TextView) findViewById(R.id.descriptionTitle)).setText(getResources().getString(R.string.description));
+        if (noxbox.getRole() == MarketRole.supply) {
+            ((TextView) findViewById(R.id.previousDescription)).setText("Готов предоставить услугу:");
+        } else {//TODO (vl) transfer text to xml
+            ((TextView) findViewById(R.id.previousDescription)).setText("Хочу получить услугу:");
         }
 
         //((TextView)findViewById(R.id.description)).setText(noxbox.getType().getDescription());
-        ((TextView)findViewById(R.id.date)).setText("Дата регистрации услуги" +" "+date(noxbox.getTimeCreated()));
-        drawDropdownElement(R.id.descriptionTitleLayout,R.id.descriptionLayout);
+        ((TextView) findViewById(R.id.date)).setText("Дата регистрации услуги" + " " + date(noxbox.getTimeCreated()));
+
     }
 
     private void drawRating(Rating rating) {
-        drawDropdownElement(R.id.ratingTitleLayout,R.id.ratingLayout);
-        ((ImageView)findViewById(R.id.ratingTitleImage)).setImageResource(R.drawable.star);
-        ((TextView)findViewById(R.id.ratingTitle)).setText(R.string.rating);
-        ((TextView)findViewById(R.id.rating)).setText(rating.toPercentage()+"%");
-        ((TextView)findViewById(R.id.like)).setText(rating.getReceivedLikes()+" like");
-        ((TextView)findViewById(R.id.dislike)).setText(rating.getReceivedDislikes()+" dislike");
+        drawDropdownElement(R.id.ratingTitleLayout, R.id.ratingLayout);
+        changeArrowVector(R.id.ratingLayout, R.id.ratingArrow);
+        ((ImageView) findViewById(R.id.ratingTitleImage)).setImageResource(R.drawable.star);
+        ((TextView) findViewById(R.id.ratingTitle)).setText(R.string.rating);
+        ((TextView) findViewById(R.id.rating)).setText(rating.toPercentage() + "%");
+        ((TextView) findViewById(R.id.like)).setText(rating.getReceivedLikes() + " like");
+        ((TextView) findViewById(R.id.dislike)).setText(rating.getReceivedDislikes() + " dislike");
 
         List<Comment> comments = new ArrayList<>();
         comments.add(rating.getComments().get("0"));
@@ -105,19 +110,21 @@ public class DetailedNoxboxPage extends AppCompatActivity {
         recyclerView.setAdapter(new CommentAdapter(comments));
     }
 
-    private void drawAvailableTime(WorkSchedule workSchedule){
-        drawDropdownElement(R.id.availableTimeTitleLayout,R.id.availableTimeLayout);
-        ((ImageView)findViewById(R.id.availableTimeImage)).setImageResource(R.drawable.clock_circle);
-        ((TextView)findViewById(R.id.availableTimeTitle)).setText(R.string.availableTime);
-        ((TextView)findViewById(R.id.currentDate)).setText("Пт 27 GMT +3");
-        ((TextView)findViewById(R.id.availableTime))
-                .setText(workSchedule.getStartTime().getHourOfDay() + ":"+ workSchedule.getStartTime().getMinuteOfHour() + " - "
-                        + workSchedule.getEndTime().getHourOfDay() + ":"+ workSchedule.getEndTime().getMinuteOfHour());
+    private void drawAvailableTime(WorkSchedule workSchedule) {
+        drawDropdownElement(R.id.availableTimeTitleLayout, R.id.availableTimeLayout);
+        changeArrowVector(R.id.availableTimeLayout, R.id.timeArrow);
+        ((ImageView) findViewById(R.id.availableTimeImage)).setImageResource(R.drawable.clock_circle);
+        ((TextView) findViewById(R.id.availableTimeTitle)).setText(R.string.availableTime);
+        ((TextView) findViewById(R.id.currentDate)).setText("Пт 27 GMT +3");
+        ((TextView) findViewById(R.id.availableTime))
+                .setText(workSchedule.getStartTime().getHourOfDay() + ":" + workSchedule.getStartTime().getMinuteOfHour() + " - "
+                        + workSchedule.getEndTime().getHourOfDay() + ":" + workSchedule.getEndTime().getMinuteOfHour());
     }
 
     private void drawPrice(Noxbox noxbox) {
-        drawDropdownElement(R.id.priceTitleLayout,R.id.priceLayout);
-        ((ImageView)findViewById(R.id.priceImage)).setImageResource(R.drawable.coin);
+        drawDropdownElement(R.id.priceTitleLayout, R.id.priceLayout);
+        changeArrowVector(R.id.priceLayout, R.id.priceArrow);
+        ((ImageView) findViewById(R.id.priceImage)).setImageResource(R.drawable.coin);
         ((TextView) findViewById(R.id.priceTitle)).setText(getResources().getString(R.string.priceTxt));
         ((TextView) findViewById(R.id.price)).setText(noxbox.getPrice());
         ((TextView) findViewById(R.id.typeTextInPrice)).setText(noxbox.getType().getName());
@@ -151,7 +158,7 @@ public class DetailedNoxboxPage extends AppCompatActivity {
         });
     }
 
-    private void drawDropdownElement(int titleId, final int contentId){
+    private void drawDropdownElement(int titleId, final int contentId) {
         findViewById(titleId).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -166,18 +173,23 @@ public class DetailedNoxboxPage extends AppCompatActivity {
         });
     }
 
-    private void init() {
-        //mTitle = (TextView) findViewById(R.id.toolbarTextView);
-        // mTitleContainer = (LinearLayout) findViewById(R.id.titleContainerLayout);
-        //mAppBarLayout = (AppBarLayout) findViewById(R.id.appBarLayout);
-        // mTypeImageView = (ImageView) findViewById(R.id.typeImageView);
-
-
-        //TODO need find background picture for all types..
-        // mBackImageTypeView = (ImageView) findViewById(R.id.backgroundImageTypeView);
-
+    private void changeArrowVector(int layout, final int element) {
+        final ViewGroup listeningLayout = findViewById(layout);
+        listeningLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (listeningLayout.getVisibility() == View.VISIBLE) {
+                    ((ImageView) findViewById(element)).setImageResource(R.drawable.arrow_up);
+                } else if (listeningLayout.getVisibility() == View.GONE) {
+                    ((ImageView) findViewById(element)).setImageResource(R.drawable.arrow_down);
+                }
+            }
+        });
 
     }
 
-
+    private void init() {
+        //TODO need find background picture for all types..
+        // mBackImageTypeView = (ImageView) findViewById(R.id.backgroundImageTypeView);
+    }
 }
