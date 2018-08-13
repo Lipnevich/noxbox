fe53151334622373c7f70e0402ee92c01f90955f
Lipnevich <Nicolay.Lipnevich@gmail.com>
2018-08-13 18:49:29 +0300

    Minor fixes

---
+++ app/src/main/java/live/noxbox/tools/NoxboxExamples.java
@@ -0,0 +1,14 @@
+package live.noxbox.tools;
+
+import java.util.List;
+
+import live.noxbox.model.Noxbox;
+import live.noxbox.model.Position;
+
+public class NoxboxExamples {
+
+    public static List<Noxbox> generateNoxbox(Position position) {
+        return null;
+    }
+
+}
