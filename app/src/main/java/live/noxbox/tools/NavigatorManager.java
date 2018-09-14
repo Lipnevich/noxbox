51c9d436d6f3aab886e0c5b7c7feeb73b8d4319e
VladislawKravchenok <vladviva5991@gmail.com>
2018-09-14 22:39:27 +0300

    class to control the launch of the navigator

---
+++ app/src/main/java/live/noxbox/tools/NavigatorManager.java
@@ -0,0 +1,55 @@
+package live.noxbox.tools;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+import live.noxbox.model.Profile;
+import live.noxbox.model.TravelMode;
+
+public class NavigatorManager {
+
+    public static void openNavigator(Activity activity, Profile profile) {
+        String uri = "google.navigation:"
+                + "q="
+                + profile.getCurrent().getPosition().getLatitude()
+                + ", "
+                + profile.getCurrent().getPosition().getLongitude();
+        if (profile.getTravelMode() == TravelMode.bicycling) {
+            uri = uri.concat("&mode=b");
+        }
+        if (profile.getTravelMode() == TravelMode.driving) {
+            uri = uri.concat("&mode=d");
+        }
+        if (profile.getTravelMode() == TravelMode.walking) {
+            uri = uri.concat("&mode=w");
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                .setPackage("com.google.android.apps.maps");
+        if (intent.resolveActivity(activity.getPackageManager()) != null) {
+            activity.startActivity(intent);
+        } else {
+            uri = "yandexnavi://build_route_on_map?"
+                    + "lat_from="
+                    + profile.getPosition().getLatitude()
+                    + "&lon_from="
+                    + profile.getPosition().getLongitude()
+                    + "&lat_to="
+                    + profile.getCurrent().getPosition().getLatitude()
+                    + "&lon_to="
+                    + profile.getCurrent().getPosition().getLongitude();
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri)).setPackage("ru.yandex.yandexnavi");
+            if (intent.resolveActivity(activity.getPackageManager()) != null) {
+                activity.startActivity(intent);
+            } else {
+
+                if (System.currentTimeMillis() % 2 == 0) {
+                    activity.startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=com.google.android.apps.maps")));
+                } else {
+                    activity.startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://details?id=ru.yandex.yandexnavi")));
+                }
+            }
+        }
+    }
+}
