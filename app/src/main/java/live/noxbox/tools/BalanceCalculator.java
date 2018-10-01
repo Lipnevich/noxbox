4ceea151f863419e361582bba0cf0b9cce9e1db4
VladislawKravchenok <vladviva5991@gmail.com>
2018-10-01 16:24:01 +0300

    java.lang.NumberFormatException BalanceCalculator.enoughBalance bug fixed

--- app/src/main/java/live/noxbox/tools/BalanceCalculator.java
+++ app/src/main/java/live/noxbox/tools/BalanceCalculator.java
@@ -6,15 +6,13 @@ import live.noxbox.model.Noxbox;
 import live.noxbox.model.Profile;
 
 public class BalanceCalculator {
+    private static final BigDecimal QUARTER = new BigDecimal("4.0");
 
     public static boolean enoughBalance(Noxbox noxbox, Profile profile) {
-        BigDecimal quarter = new BigDecimal("4.0");
         BigDecimal minimalPrice = new BigDecimal(noxbox.getPrice());
-        minimalPrice = minimalPrice.divide(quarter, 2, BigDecimal.ROUND_HALF_UP);
+        minimalPrice = minimalPrice.divide(QUARTER, 2, BigDecimal.ROUND_HALF_UP);
         BigDecimal walletBalance = new BigDecimal(profile.getWallet().getBalance());
 
-        if (minimalPrice.compareTo(walletBalance) < 0) return true;
-
-        return false;
+        return minimalPrice.compareTo(walletBalance) < 0;
     }
 }
