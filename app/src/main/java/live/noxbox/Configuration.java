1651775a82343fd6ccaac05252184db477b11720
VladislawKravchenok <vladviva5991@gmail.com>
2018-10-10 03:11:10 +0300

    Minor cleaning

--- app/src/main/java/live/noxbox/Configuration.java
+++ app/src/main/java/live/noxbox/Configuration.java
@@ -4,7 +4,6 @@ public interface Configuration {
     int REQUESTING_AND_ACCEPTING_TIMEOUT_IN_SECONDS = 60;
     int REQUESTING_AND_ACCEPTING_TIMEOUT_IN_MILLIS = 60 * 1000;
     String START_TIME = "00:00:00";
-    int ACCEPTANCE_TIMEOUT_IN_SECONDS = 30;
     double RADIUS_IN_METERS = 50 * 1000;
     int MIN_RATE_IN_PERCENTAGE = 90;
     String CURRENCY = "Waves";
