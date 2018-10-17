846a9a7174ec9ffe7ea66be7ca8189b19f940230
VladislawKravchenok <vladviva5991@gmail.com>
2018-10-17 17:52:46 +0300

    Увеличено кол-во символов после запятой при вычислениях баланса

--- app/src/main/java/live/noxbox/Configuration.java
+++ app/src/main/java/live/noxbox/Configuration.java
@@ -14,7 +14,7 @@ public interface Configuration {
     float MINIMUM_FACE_SIZE = 0.6F;
     float MINIMUM_PROBABILITY_FOR_ACCEPTANCE = 0.6F;
     String FIVE_MINUTES_PART_OF_HOUR = "12";
-    int DEFAULT_BALANCE_SCALE = 3;
+    int DEFAULT_BALANCE_SCALE = 10;
     String PRICE_FORMAT = "###.###";
     BigDecimal QUARTER = new BigDecimal("0.25");
 
