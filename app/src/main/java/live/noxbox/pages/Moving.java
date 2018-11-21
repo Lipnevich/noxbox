3aaec4aedde12e1c23de6a1bef8b07889027f2a6
VladislawKravchenok <vladviva5991@gmail.com>
2018-11-21 11:44:52 +0300

    Для подтверждения фотографии, в состоянии Moving, создана отдельная активность ConfirmationActivity

--- app/src/main/java/live/noxbox/pages/Moving.java
+++ app/src/main/java/live/noxbox/pages/Moving.java
@@ -2,20 +2,13 @@ package live.noxbox.pages;
 
 import android.app.Activity;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.CountDownTimer;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -27,15 +20,15 @@ import live.noxbox.model.NotificationType;
 import live.noxbox.model.Profile;
 import live.noxbox.model.TravelMode;
 import live.noxbox.notifications.util.MessagingService;
+import live.noxbox.pages.confirmation.ConfirmationActivity;
 import live.noxbox.state.State;
 import live.noxbox.tools.DateTimeFormatter;
 import live.noxbox.tools.DebugMessage;
 import live.noxbox.tools.MapController;
 import live.noxbox.tools.MarkerCreator;
 import live.noxbox.tools.NavigatorManager;
-import live.noxbox.tools.Task;
+import live.noxbox.tools.Router;
 
-import static live.noxbox.database.AppCache.updateNoxbox;
 import static live.noxbox.tools.MapController.moveCopyrightLeft;
 import static live.noxbox.tools.MapController.moveCopyrightRight;
 import static live.noxbox.tools.Router.startActivity;
@@ -45,11 +38,9 @@ public class Moving implements State {
     private GoogleMap googleMap;
     private Activity activity;
     private LinearLayout movingView;
-    private LinearLayout photoView;
 
     private CountDownTimer countDownTimer;
 
-    private static boolean acceptingViewIsOpened = false;
 
     public Moving(final GoogleMap googleMap, final Activity activity) {
         this.googleMap = googleMap;
@@ -72,8 +63,8 @@ public class Moving implements State {
         MarkerCreator.createPartyMarker(profile.getCurrent().getParty(), googleMap, activity.getResources());
 
         activity.findViewById(R.id.menu).setVisibility(View.VISIBLE);
-        activity.findViewById(R.id.navigation).setVisibility(View.VISIBLE);
         activity.findViewById(R.id.chat).setVisibility(View.VISIBLE);
+        activity.findViewById(R.id.navigation).setVisibility(View.VISIBLE);
         activity.findViewById(R.id.locationButton).setVisibility(View.VISIBLE);
 
         if (profile.getCurrent().getOwner().equals(profile)) {
@@ -96,9 +87,6 @@ public class Moving implements State {
         View childMoving = activity.getLayoutInflater().inflate(R.layout.state_moving, null);
         movingView.addView(childMoving);
 
-        if (acceptingViewIsOpened) {
-            openAcceptingView(profile);
-        }
 
         activity.findViewById(R.id.navigation).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,7 +107,7 @@ public class Moving implements State {
         activity.findViewById(R.id.customFloatingView).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openAcceptingView(profile);
+                Router.startActivity(activity, ConfirmationActivity.class);
             }
         });
 
@@ -146,7 +134,7 @@ public class Moving implements State {
             countDownTimer = new CountDownTimer(profile.getCurrent().getTimeToMeet(), 1000) {
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    if(countDownTimer != null){
+                    if (countDownTimer != null) {
                         notification.setTime(String.valueOf(profile.getCurrent().getTimeToMeet() - (profile.getCurrent().getTimeToMeet() - millisUntilFinished)));
                         notification.setProgress((int) (profile.getCurrent().getTimeToMeet() - millisUntilFinished));
                         //NotificationType.updateNotification(activity.getApplicationContext(), notification, MessagingService.builder);
@@ -173,117 +161,17 @@ public class Moving implements State {
         MapController.clearMapMarkerListener(googleMap);
         NotificationType.removeNotifications(activity.getApplicationContext());
         googleMap.getUiSettings().setScrollGesturesEnabled(true);
+        activity.findViewById(R.id.menu).setVisibility(View.GONE);
+        activity.findViewById(R.id.chat).setVisibility(View.GONE);
         activity.findViewById(R.id.navigation).setVisibility(View.GONE);
         activity.findViewById(R.id.customFloatingView).setVisibility(View.GONE);
-        ((FloatingActionButton) activity.findViewById(R.id.customFloatingView)).setImageResource(R.drawable.add);
-        activity.findViewById(R.id.chat).setVisibility(View.GONE);
         activity.findViewById(R.id.locationButton).setVisibility(View.GONE);
         moveCopyrightLeft(googleMap);
-        activity.findViewById(R.id.menu).setVisibility(View.GONE);
         googleMap.clear();
 
-        movingView.removeAllViews();
-        if (photoView != null) {
-            photoView.removeAllViews();
+        if (movingView != null) {
+            movingView.removeAllViews();
         }
-
-    }
-
-    private void openAcceptingView(final Profile profile) {
-        acceptingViewIsOpened = true;
-        googleMap.getUiSettings().setScrollGesturesEnabled(false);
-
-        activity.findViewById(R.id.menu).setVisibility(View.GONE);
-        activity.findViewById(R.id.chat).setVisibility(View.GONE);
-        activity.findViewById(R.id.navigation).setVisibility(View.GONE);
-        activity.findViewById(R.id.customFloatingView).setVisibility(View.GONE);
-        activity.findViewById(R.id.locationButton).setVisibility(View.GONE);
-
-        photoView = movingView.findViewById(R.id.photoContainer);
-        final View childPhoto = activity.getLayoutInflater().inflate(R.layout.state_moving_photo, null);
-        photoView.addView(childPhoto);
-
-        if (profile.getId().equals(profile.getCurrent().getOwner().getId())) {
-            Glide.with(activity).asDrawable().load(profile.getCurrent().getParty().getPhoto()).into(new SimpleTarget<Drawable>() {
-                @Override
-                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                    ((ImageView) photoView.findViewById(R.id.photo)).setImageDrawable(resource);
-                }
-            });
-        } else {
-            Glide.with(activity).asDrawable().load(profile.getCurrent().getOwner().getPhoto()).into(new SimpleTarget<Drawable>() {
-                @Override
-                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
-                    ((ImageView) photoView.findViewById(R.id.photo)).setImageDrawable(resource);
-                }
-            });
-        }
-
-        photoView.findViewById(R.id.photoScreenClose).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                acceptingViewIsOpened = false;
-                googleMap.getUiSettings().setScrollGesturesEnabled(true);
-
-                activity.findViewById(R.id.menu).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.customFloatingView).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.chat).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.navigation).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.locationButton).setVisibility(View.VISIBLE);
-                photoView.removeAllViews();
-            }
-        });
-
-
-        final SwipeButton buttonConformity = photoView.findViewById(R.id.swipeButtonWrongPhoto);
-        buttonConformity.setParametrs(activity.getDrawable(R.drawable.no), activity.getResources().getString(R.string.notLikeThat), activity);
-        buttonConformity.setOnTouchListener(buttonConformity.getButtonTouchListener(new Task<Object>() {
-            @Override
-            public void execute(Object object) {
-                acceptingViewIsOpened = false;
-                photoView.removeAllViews();
-                long timeCanceled = System.currentTimeMillis();
-                if (profile.getCurrent().getOwner().getId().equals(profile.getId())) {
-                    Log.d(TAG + "Moving", "timeCanceledByOwner: " + DateTimeFormatter.time(timeCanceled));
-                    profile.getCurrent().setTimeCanceledByOwner(timeCanceled);
-                } else {
-                    Log.d(TAG + "Moving", "timeCanceledByParty: " + DateTimeFormatter.time(timeCanceled));
-                    profile.getCurrent().setTimeCanceledByParty(timeCanceled);
-                }
-                updateNoxbox();
-            }
-        }));
-
-
-        final SwipeButton buttonConfirm = photoView.findViewById(R.id.swipeButtonConfirm);
-        buttonConfirm.setParametrs(activity.getDrawable(R.drawable.yes), activity.getResources().getString(R.string.confirm), activity);
-        buttonConfirm.setOnTouchListener(buttonConfirm.getButtonTouchListener(new Task<Object>() {
-            @Override
-            public void execute(Object object) {
-                acceptingViewIsOpened = false;
-                googleMap.getUiSettings().setScrollGesturesEnabled(true);
-
-                activity.findViewById(R.id.menu).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.customFloatingView).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.chat).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.navigation).setVisibility(View.VISIBLE);
-                activity.findViewById(R.id.locationButton).setVisibility(View.VISIBLE);
-
-                long timeVerified = System.currentTimeMillis();
-                if (profile.getCurrent().getOwner().getId().equals(profile.getId())) {
-                    Log.d(TAG + "Moving", "timeOwnerVerified: " + DateTimeFormatter.time(timeVerified));
-                    profile.getCurrent().setTimeOwnerVerified(timeVerified);
-                } else {
-                    Log.d(TAG + "Moving", "timePartyVerified: " + DateTimeFormatter.time(timeVerified));
-                    profile.getCurrent().setTimePartyVerified(timeVerified);
-                }
-                AppCache.updateNoxbox();
-
-                if (photoView != null)
-                    photoView.removeAllViews();
-            }
-        }));
-
     }
 
     private Float getDistance(Profile profile) {
---
+++ app/src/main/java/live/noxbox/pages/confirmation/ConfirmationActivity.java
@@ -0,0 +1,122 @@
+package live.noxbox.pages.confirmation;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import live.noxbox.BaseActivity;
+import live.noxbox.R;
+import live.noxbox.database.AppCache;
+import live.noxbox.model.Profile;
+import live.noxbox.pages.SwipeButton;
+import live.noxbox.tools.DateTimeFormatter;
+import live.noxbox.tools.Task;
+
+import static live.noxbox.database.AppCache.updateNoxbox;
+
+public class ConfirmationActivity extends BaseActivity {
+
+    private static final String TAG = ConfirmationActivity.class.getName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppCache.readProfile(new Task<Profile>() {
+            @Override
+            public void execute(Profile profile) {
+                draw(profile);
+            }
+        });
+    }
+
+    private void draw(Profile profile) {
+        drawToolbar();
+        drawPhoto(profile, this);
+        drawConfirmButton(profile, this);
+        drawConformityButton(profile, this);
+    }
+
+    private void drawToolbar() {
+        ((TextView) findViewById(R.id.title)).setText(R.string.verification);
+        findViewById(R.id.homeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void drawPhoto(Profile profile, Activity activity) {
+        if (profile.getId().equals(profile.getCurrent().getOwner().getId())) {
+            Glide.with(activity).asDrawable().load(profile.getCurrent().getParty().getPhoto()).into(new SimpleTarget<Drawable>() {
+                @Override
+                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                    ((ImageView) findViewById(R.id.photo)).setImageDrawable(resource);
+                }
+            });
+        } else {
+            Glide.with(activity).asDrawable().load(profile.getCurrent().getOwner().getPhoto()).into(new SimpleTarget<Drawable>() {
+                @Override
+                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                    ((ImageView) findViewById(R.id.photo)).setImageDrawable(resource);
+                }
+            });
+        }
+    }
+
+    private void drawConfirmButton(final Profile profile, final Activity activity) {
+        final SwipeButton buttonConfirm = findViewById(R.id.swipeButtonConfirm);
+        buttonConfirm.setParametrs(activity.getDrawable(R.drawable.yes), activity.getResources().getString(R.string.confirm), activity);
+        buttonConfirm.setOnTouchListener(buttonConfirm.getButtonTouchListener(new Task<Object>() {
+            @Override
+            public void execute(Object object) {
+                long timeVerified = System.currentTimeMillis();
+                if (profile.getCurrent().getOwner().getId().equals(profile.getId())) {
+                    Log.d(TAG, "timeOwnerVerified: " + DateTimeFormatter.time(timeVerified));
+                    profile.getCurrent().setTimeOwnerVerified(timeVerified);
+                } else {
+                    Log.d(TAG, "timePartyVerified: " + DateTimeFormatter.time(timeVerified));
+                    profile.getCurrent().setTimePartyVerified(timeVerified);
+                }
+                updateNoxbox();
+                finish();
+            }
+        }));
+    }
+
+    private void drawConformityButton(final Profile profile, Activity activity) {
+        SwipeButton buttonConformity = findViewById(R.id.swipeButtonWrongPhoto);
+        buttonConformity.setParametrs(activity.getDrawable(R.drawable.no), activity.getResources().getString(R.string.notLikeThat), activity);
+        buttonConformity.setOnTouchListener(buttonConformity.getButtonTouchListener(new Task<Object>() {
+            @Override
+            public void execute(Object object) {
+                long timeCanceled = System.currentTimeMillis();
+                if (profile.getCurrent().getOwner().getId().equals(profile.getId())) {
+                    Log.d(TAG, "timeCanceledByOwner: " + DateTimeFormatter.time(timeCanceled));
+                    profile.getCurrent().setTimeCanceledByOwner(timeCanceled);
+                } else {
+                    Log.d(TAG, "timeCanceledByParty: " + DateTimeFormatter.time(timeCanceled));
+                    profile.getCurrent().setTimeCanceledByParty(timeCanceled);
+                }
+                updateNoxbox();
+                finish();
+            }
+        }));
+    }
+}
