059dafef3b986b690aefc8953a70f451b1bdaf64
VladislawKravchenok <vladviva5991@gmail.com>
2018-12-12 08:30:27 +0300

    Яндекс навигатор установлен по умолчанию

--- app/src/main/java/live/noxbox/tools/NavigatorManager.java
+++ app/src/main/java/live/noxbox/tools/NavigatorManager.java
@@ -10,36 +10,36 @@ import live.noxbox.model.TravelMode;
 public class NavigatorManager {
 
     public static void openNavigator(Context context, Profile profile) {
-        String uri = "google.navigation:"
-                + "q="
+        String uri = "yandexnavi://build_route_on_map?"
+                + "lat_from="
+                + profile.getPosition().getLatitude()
+                + "&lon_from="
+                + profile.getPosition().getLongitude()
+                + "&lat_to="
                 + profile.getCurrent().getPosition().getLatitude()
-                + ", "
+                + "&lon_to="
                 + profile.getCurrent().getPosition().getLongitude();
-        if (profile.getTravelMode() == TravelMode.bicycling) {
-            uri = uri.concat("&mode=b");
-        }
-        if (profile.getTravelMode() == TravelMode.driving) {
-            uri = uri.concat("&mode=d");
-        }
-        if (profile.getTravelMode() == TravelMode.walking) {
-            uri = uri.concat("&mode=w");
-        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri)).setPackage("ru.yandex.yandexnavi");
 
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri))
-                .setPackage("com.google.android.apps.maps");
         if (intent.resolveActivity(context.getPackageManager()) != null) {
             context.startActivity(intent);
         } else {
-            uri = "yandexnavi://build_route_on_map?"
-                    + "lat_from="
-                    + profile.getPosition().getLatitude()
-                    + "&lon_from="
-                    + profile.getPosition().getLongitude()
-                    + "&lat_to="
+            uri = "google.navigation:"
+                    + "q="
                     + profile.getCurrent().getPosition().getLatitude()
-                    + "&lon_to="
+                    + ", "
                     + profile.getCurrent().getPosition().getLongitude();
-            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri)).setPackage("ru.yandex.yandexnavi");
+            if (profile.getTravelMode() == TravelMode.bicycling) {
+                uri = uri.concat("&mode=b");
+            }
+            if (profile.getTravelMode() == TravelMode.driving) {
+                uri = uri.concat("&mode=d");
+            }
+            if (profile.getTravelMode() == TravelMode.walking) {
+                uri = uri.concat("&mode=w");
+            }
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                    .setPackage("com.google.android.apps.maps");
             if (intent.resolveActivity(context.getPackageManager()) != null) {
                 context.startActivity(intent);
             } else {
