7fe5b2150e1d167ed247f061101f9aefe1a7d691
VladislawKravchenok <vladviva5991@gmail.com>
2018-12-17 16:07:52 +0300

    История перекочевала с ListView в RecyclerView

--- app/src/main/java/live/noxbox/menu/HistoryActivity.java
+++ app/src/main/java/live/noxbox/menu/HistoryActivity.java
@@ -2,46 +2,20 @@ package live.noxbox.menu;
 
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.AbsListView;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import live.noxbox.R;
 import live.noxbox.activities.BaseActivity;
-import live.noxbox.database.Firestore;
-import live.noxbox.model.MarketRole;
 import live.noxbox.model.Noxbox;
-import live.noxbox.tools.Task;
 
 public class HistoryActivity extends BaseActivity {
 
     public static final int CODE = 1002;
-    private static final int AMOUNT_PER_LOAD = 20;
-    private int requestedAmount;
-
-    private HistoryAdapter historyAdapter;
     private List<Noxbox> historyItems = new ArrayList<>();
-    private ListView listView;
-    private ImageView progressView;
-
-    private Task<Collection<Noxbox>> uploadingTask = new Task<Collection<Noxbox>>() {
-        @Override
-        public void execute(Collection<Noxbox> items) {
-            historyItems.addAll(items);
-            progressView.setVisibility(View.INVISIBLE);
-            listView.setVisibility(View.VISIBLE);
-            historyAdapter.notifyDataSetChanged();
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,36 +23,18 @@ public class HistoryActivity extends BaseActivity {
         setContentView(R.layout.activity_history);
         setTitle(R.string.history);
 
-        int size = 384;
-        progressView = findViewById(R.id.progressView);
-        Glide.with(this).asGif()
-                .load(R.drawable.progress_cat)
-                .apply(RequestOptions.overrideOf(size, size))
-                .into(progressView);
+//        int size = 384;
+//        progressView = findViewById(R.id.progressView);
+//        Glide.with(this).asGif()
+//                .load(R.drawable.progress_cat)
+//                .apply(RequestOptions.overrideOf(size, size))
+//                .into(progressView);
 
-        listView = findViewById(R.id.historyView);
-        historyAdapter = new HistoryAdapter(HistoryActivity.this, historyItems);
-        listView.setAdapter(historyAdapter);
-        RecyclerView recyclerView = new RecyclerView(HistoryActivity.this);
+        // TODO (vl) use MarketRole.demand for payer's activity_history
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter( new HistoryAdapter(this, historyItems, recyclerView));
 
-
-        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) { }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                if (firstVisibleItem >= requestedAmount - visibleItemCount * 2) {
-                    long startFrom = historyItems.isEmpty() ?
-                            Long.MAX_VALUE :
-                            historyItems.get(historyItems.size() - 1).getTimeCompleted();
-
-                    // TODO (vl) use MarketRole.demand for payer's activity_history
-                    Firestore.readHistory(startFrom, AMOUNT_PER_LOAD, MarketRole.supply, uploadingTask);
-                    requestedAmount += AMOUNT_PER_LOAD;
-                }
-            }
-        });
     }
 
 }
--- app/src/main/java/live/noxbox/menu/HistoryAdapter.java
+++ app/src/main/java/live/noxbox/menu/HistoryAdapter.java
@@ -3,117 +3,176 @@ package live.noxbox.menu;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.GroundOverlay;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import live.noxbox.R;
+import live.noxbox.database.Firestore;
+import live.noxbox.model.MarketRole;
 import live.noxbox.model.Noxbox;
+import live.noxbox.tools.Task;
 
 import static live.noxbox.tools.DateTimeFormatter.date;
-import static live.noxbox.tools.DateTimeFormatter.time;
 
 /**
  * Created by nicolay.lipnevich on 22/06/2017.
  */
 
-public class HistoryAdapter extends BaseAdapter {
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder> {
 
-    Context ctx;
-    LayoutInflater inflater;
+    private Context ctx;
     private String profileId;
-    List<Noxbox> noxboxes;
+    private List<Noxbox> historyItems;
+    private Set<Noxbox> uniqueValue = new HashSet<>();
+    private int lastVisibleItem, totalItemCount;
+    private static final int AMOUNT_PER_LOAD = 10;
 
-    public HistoryAdapter(Context context, List<Noxbox> noxboxes) {
+
+    public HistoryAdapter(Context context, final List<Noxbox> historyItems, RecyclerView currentRecyclerView) {
         this.ctx = context;
         this.profileId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        this.noxboxes = noxboxes;
-        this.inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
+        this.historyItems = historyItems;
 
-    @Override
-    public int getCount() {
-        return noxboxes.size();
-    }
+        long startFrom = historyItems.isEmpty() ?
+                Long.MAX_VALUE :
+                historyItems.get(historyItems.size() - 1).getTimeCompleted();
 
-    @Override
-    public Object getItem(int position) {
-        return noxboxes.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, final View convertView, ViewGroup parent) {
-        final View view = convertView != null ? convertView : inflater.inflate(R.layout.item_history, parent, false);
-
-        final Noxbox noxbox = (Noxbox) getItem(position);
-
-        ((TextView) view.findViewById(R.id.dateText)).setText(date(noxbox.getTimeCompleted()));
-        ((TextView) view.findViewById(R.id.timeText)).setText(time(noxbox.getTimeCompleted()));
-        ((TextView) view.findViewById(R.id.priceText)).setText(noxbox.getPrice());
-        ((TextView) view.findViewById(R.id.performerName)).setText(noxbox.getParty().getName());
-
-        MapView mapView = view.findViewById(R.id.map);
-        mapView.onCreate(null);
-        mapView.onResume();
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        final Task<Collection<Noxbox>> uploadingTask = new Task<Collection<Noxbox>>() {
             @Override
-            public void onMapReady(GoogleMap googleMap) {
-                MapsInitializer.initialize(view.getContext());
-                GroundOverlayOptions newarkMap = new GroundOverlayOptions()
-                        .image(BitmapDescriptorFactory.fromResource(noxbox.getType().getImage()))
-                        .position(noxbox.getPosition().toLatLng(), 48, 48)
-                        .anchor(0.5f, 1)
-                        .zIndex(1000);
-                GroundOverlay marker = googleMap.addGroundOverlay(newarkMap);
-                marker.setDimensions(960, 960);
-
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(noxbox.getPosition().toLatLng(), 11));
+            public void execute(Collection<Noxbox> items) {
+                List<Noxbox> noxboxes = new ArrayList<>();
+                for (Noxbox noxbox : items) {
+                    if (!uniqueValue.contains(noxbox)) {
+                        uniqueValue.add(noxbox);
+                        noxboxes.add(noxbox);
+                    }
+                }
+                if (noxboxes.size() > 0) {
+                    historyItems.addAll(noxboxes);
+                    notifyDataSetChanged();
+                }
             }
-        });
+        };
+        Firestore.readHistory(startFrom, AMOUNT_PER_LOAD, MarketRole.supply, uploadingTask);
 
-        Glide.with(view.getContext())
+        if (currentRecyclerView.getLayoutManager() instanceof LinearLayoutManager) {
+            final LinearLayoutManager linearLayoutManager = (LinearLayoutManager) currentRecyclerView.getLayoutManager();
+            currentRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    totalItemCount = linearLayoutManager.getItemCount();
+                    lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition();
+                    if (totalItemCount <= (lastVisibleItem + AMOUNT_PER_LOAD)) {
+
+                        long startFrom = historyItems.isEmpty() ?
+                                Long.MAX_VALUE :
+                                historyItems.get(historyItems.size() - 1).getTimeCompleted();
+                        // TODO (vl) use MarketRole.demand for payer's activity_history
+                        Firestore.readHistory(startFrom, AMOUNT_PER_LOAD, MarketRole.supply, uploadingTask);
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return historyItems.get(position) != null ? 1 : 0;
+    }
+
+    @NonNull
+    @Override
+    public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        HistoryViewHolder viewHolder;
+        if (viewType == 1) {
+            viewHolder = new ItemViewHolder(LayoutInflater.from(ctx).inflate(R.layout.item_history, parent, false));
+        } else {
+            viewHolder = new ProgressViewHolder(LayoutInflater.from(ctx).inflate(R.layout.item_progress, parent, false));
+        }
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryViewHolder viewHolder, int position) {
+        if (viewHolder instanceof ProgressViewHolder) {
+            viewHolder.progressBar.setIndeterminate(true);
+            return;
+        }
+
+        final Noxbox noxbox = historyItems.get(position);
+
+        if (noxbox.getTimeCompleted() != null) {
+            viewHolder.time.setText(date(noxbox.getTimeCompleted()));
+            viewHolder.date.setText(date(noxbox.getTimeCompleted()));
+        } else if (noxbox.getTimeCanceledByParty() != null) {
+            viewHolder.time.setText(date(noxbox.getTimeCanceledByParty()));
+            viewHolder.date.setText(date(noxbox.getTimeCanceledByParty()));
+        } else if (noxbox.getTimeCanceledByOwner() != null) {
+            viewHolder.time.setText(date(noxbox.getTimeCanceledByOwner()));
+            viewHolder.date.setText(date(noxbox.getTimeCanceledByOwner()));
+        } else if (noxbox.getTimeTimeout() != null) {
+            viewHolder.date.setText(date(noxbox.getTimeTimeout()));
+            viewHolder.date.setText(date(noxbox.getTimeTimeout()));
+        }
+
+        viewHolder.price.setText(noxbox.getPrice());
+        viewHolder.performerName.setText(noxbox.getNotMe(profileId).getName());
+
+        //viewHolder.mapView.onCreate(null);
+        //viewHolder.mapView.onResume();
+//        viewHolder.mapView.getMapAsync(new OnMapReadyCallback() {
+//            @Override
+//            public void onMapReady(GoogleMap googleMap) {
+//                MapsInitializer.initialize(ctx);
+//                GroundOverlayOptions newarkMap = new GroundOverlayOptions()
+//                        .image(BitmapDescriptorFactory.fromResource(noxbox.getType().getImage()))
+//                        .position(noxbox.getPosition().toLatLng(), 48, 48)
+//                        .anchor(0.5f, 1)
+//                        .zIndex(1000);
+//                GroundOverlay marker = googleMap.addGroundOverlay(newarkMap);
+//                marker.setDimensions(960, 960);
+//
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(noxbox.getPosition().toLatLng(), 11));
+//            }
+//        });
+
+        Glide.with(ctx)
                 .load(noxbox.getNotMe(profileId).getPhoto())
                 .apply(RequestOptions.circleCropTransform())
-                .into((ImageView) view.findViewById(R.id.performerImage));
+                .into(viewHolder.performerPhoto);
 
         boolean isLiked = isLiked(noxbox);
-
-        final ImageView rateBox = view.findViewById(R.id.rateBox);
-        showRating(rateBox, isLiked);
-        if(isLiked) {
-            rateBox.setOnClickListener(new View.OnClickListener() {
+        showRating(viewHolder.rateNoxbox, isLiked);
+        if (isLiked) {
+            viewHolder.rateNoxbox.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(final View v) {
+                public void onClick(final View view) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(ctx, R.style.NoxboxAlertDialogStyle);
                     builder.setTitle(ctx.getResources().getString(R.string.dislikePrompt));
                     builder.setPositiveButton(ctx.getResources().getString(R.string.dislike),
                             new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
-                                    v.setOnClickListener(null);
-                                    showRating((ImageView) v, false);
+                                    view.setOnClickListener(null);
+                                    showRating((ImageView) view, false);
 //                                    dislikeNoxbox(profileId, noxbox);
                                 }
                             });
@@ -122,11 +181,57 @@ public class HistoryAdapter extends BaseAdapter {
                 }
             });
         }
-        return view;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return historyItems.size();
+    }
+
+    static class HistoryViewHolder extends RecyclerView.ViewHolder {
+        //ItemViewHolder
+        TextView date;
+        TextView time;
+        TextView price;
+        ImageView performerPhoto;
+        TextView performerName;
+        //MapView mapView;
+        ImageView rateNoxbox;
+
+        //ProgressViewHolder
+        ProgressBar progressBar;
+
+        public HistoryViewHolder(@NonNull View layout) {
+            super(layout);
+        }
+    }
+
+    static class ItemViewHolder extends HistoryViewHolder {
+
+        public ItemViewHolder(@NonNull View layout) {
+            super(layout);
+            date = layout.findViewById(R.id.dateText);
+            time = layout.findViewById(R.id.timeText);
+            price = layout.findViewById(R.id.priceText);
+            performerPhoto = layout.findViewById(R.id.performerImage);
+            performerName = layout.findViewById(R.id.performerName);
+            //mapView = layout.findViewById(R.id.map);
+            rateNoxbox = layout.findViewById(R.id.rateBox);
+        }
+    }
+
+    static class ProgressViewHolder extends HistoryViewHolder {
+
+
+        public ProgressViewHolder(@NonNull View layout) {
+            super(layout);
+            progressBar = layout.findViewById(R.id.progressBar);
+        }
     }
 
     private boolean isLiked(Noxbox noxbox) {
-        if(profileId.equals(noxbox.getOwner().getId())) {
+        if (profileId.equals(noxbox.getOwner().getId())) {
             return noxbox.getTimeOwnerDisliked() == null;
         } else {
             return noxbox.getTimePartyDisliked() == null;
@@ -134,11 +239,10 @@ public class HistoryAdapter extends BaseAdapter {
     }
 
     private void showRating(ImageView view, boolean isLiked) {
-        if(isLiked) {
+        if (isLiked) {
             view.setImageResource(R.drawable.like);
         } else {
             view.setImageResource(R.drawable.dislike);
         }
     }
-
 }
