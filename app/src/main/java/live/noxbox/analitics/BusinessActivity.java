3a19bf453ecc1e25c89457c7dbe60d0d2e56abee
Lipnevich <Nicolay.Lipnevich@gmail.com>
2019-02-08 12:11:00 +0300

    С ошибками. Настройка событий в приложении.

---
+++ app/src/main/java/live/noxbox/analitics/BusinessActivity.java
@@ -0,0 +1,13 @@
+package live.noxbox.analitics;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+
+public class BusinessActivity extends AppCompatActivity {
+
+    public static SharedPreferences writes;
+    public static SharedPreferences reads;
+    private static Context context;
+
+}
