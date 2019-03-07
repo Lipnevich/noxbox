b76af731fcc680cce178a8e84bc491c9ba1dc26e
Lipnevich <Nicolay.Lipnevich@gmail.com>
2019-03-07 11:59:36 +0300

    Hide system Navigation Bar

--- app/src/main/java/live/noxbox/activities/BaseActivity.java
+++ app/src/main/java/live/noxbox/activities/BaseActivity.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -22,6 +24,11 @@ public abstract class BaseActivity extends BusinessActivity {
 
     private SharedPreferences firstRunPreference;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
 
     @Override
     protected void onResume() {
