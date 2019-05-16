e804d9275f6037f4878f7df281da8df32ea9b7e1
VladislawKravchenok <vladviva5991@gmail.com>
2019-05-16 10:31:15 +0300

    Code clean up

--- app/src/main/java/live/noxbox/model/Acceptance.java
+++ app/src/main/java/live/noxbox/model/Acceptance.java
@@ -29,7 +29,7 @@ public class Acceptance {
                 + "rightEyeOpenProbability: " + rightEyeOpenProbability + ", "
                 + "leftEyeOpenProbability: " + leftEyeOpenProbability + ", "
                 + "faceSize: " + faceSize + ", "
-                + "message: " + message + ".";
+                + "message: " + message;
     }
 
     @Exclude
